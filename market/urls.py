1250bf988b80289aabe46450461373deb46d212e
YonghoBae <dyddyd134@chungbuk.ac.kr>
2023-05-30 15:11:29 +0900

    test

--- market/urls.py
+++ market/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import base_views, Product_views, Commet_views
+from .views import base_views, product_views, comment_views
 
 app_name = 'market'
 
@@ -9,15 +9,15 @@ urlpatterns = [
     path('', base_views.index, name='index'),
     path('<int:Product_id>/', base_views.detail, name='detail'),
 
-    # Product_views.py
-    path('Product/create/', Product_views.Product_create, name='Product_create'),
-    path('Product/modify/<int:Product_id>/', Product_views.Product_modify, name='Product_modify'),
-    path('Product/delete/<int:Product_id>/', Product_views.Product_delete, name='Product_delete'),
+    # product_views.py
+    path('Product/create/', product_views.Product_create, name='Product_create'),
+    path('Product/modify/<int:Product_id>/', product_views.Product_modify, name='Product_modify'),
+    path('Product/delete/<int:Product_id>/', product_views.Product_delete, name='Product_delete'),
 
-    # Commet_views.py
-    path('Comment/create/<int:Product_id>/', Commet_views.Comment_create, name='Comment_create'),
-    path('Comment/modify/<int:Comment_id>/', Commet_views.Comment_modify, name='Comment_modify'),
-    path('Comment/delete/<int:Comment_id>/', Comment_views.Comment_delete, name='Comment_delete'),
-    path('Product/vote/<int:Product_id>/', Product_views.Product_vote, name='Product_vote'),
-    path('vote/Comment/<int:Comment_id>/', Commet_views.Comment_vote, name='Comment_vote'),
+    # comment_views.py
+    path('Comment/create/<int:Product_id>/', comment_views.Comment_create, name='Comment_create'),
+    path('Comment/modify/<int:Comment_id>/', comment_views.Comment_modify, name='Comment_modify'),
+    path('Comment/delete/<int:Comment_id>/', comment_views.Comment_delete, name='Comment_delete'),
+    path('Product/vote/<int:Product_id>/', product_views.Product_vote, name='Product_vote'),
+    path('vote/Comment/<int:Comment_id>/', comment_views.Comment_vote, name='Comment_vote'),
 ]
