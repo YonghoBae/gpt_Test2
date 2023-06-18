bb720548e3db3070a49faaed9307cbe5f6bcea31
SDH1001 <74577758+SDH1001@users.noreply.github.com>
2023-06-18 09:29:42 +0900

    Update urls.py

--- market/urls.py
+++ market/urls.py
@@ -5,9 +5,11 @@ app_name = 'market'
 
 urlpatterns = [
     # base_views.py
-    path('', base_views.index, name='index'),
+    path('Product/', base_views.index, name='index'),
     path('<int:product_id>/', base_views.detail, name='detail'),
-
+    
+    path('', base_views.main, name='main'),
+    
     # product_views.py
     path('Product/create/', product_views.Product_create, name='Product_create'),
     path('Product/modify/<int:product_id>/', product_views.Product_modify, name='Product_modify'),
